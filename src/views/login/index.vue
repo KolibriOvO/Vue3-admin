0a69818b8a3fd1b804d8845316d2e70a3774d8af
kolibri <vigilanteovo@gmail.com>
2023-08-02 21:28:05 +0800

    登录校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,25 +1,31 @@
 <script setup lang="ts">
 import {Lock, User} from "@element-plus/icons-vue";
-import {reactive, ref} from "vue";
+import {ErrorCodes, reactive, ref} from "vue";
 import useUserStore from "@/store/modules/user.ts";
 import {useRouter} from "vue-router";
 import {ElNotification} from "element-plus";
+import {getTime} from "@/utils/time.ts";
 
 let useStore = useUserStore()
 
 let $router = useRouter()
 
-let loginForm = reactive({username: '', password: ''})
+let loginForm = reactive({username: 'admin', password: '123456'})
 
 let loading = ref(false)
+
+let loginForms = ref()
+
 const login = async () => {
+  await loginForms.value.validate()
   loading.value = true
   try {
     await useStore.userLogin(loginForm);
     $router.push('/')
     ElNotification({
       type: 'success',
-      message: '登陆成功'
+      message: '欢迎回来',
+      title: `HI,${getTime()}好`
     })
     loading.value = false
   } catch (error) {
@@ -30,6 +36,32 @@ const login = async () => {
     })
   }
 }
+const validatorUserName = (rule:any,value:any,callback:any)=>{
+  if (/^\d{5,10}$/.test(value)){
+    callback()
+  }else{
+    callback(new Error('账号5-10位'))
+  }
+}
+const validatorPassword = (rule:any,value:any,callback:any)=>{
+  if (value.length>=6){
+    callback()
+  }else{
+    callback(new Error('密码至少6位'))
+  }
+}
+const rules = {
+  username: [
+    // {required: true, message: '用户名不能为空', trigger: 'blur'},
+    // {required: true, min: 6, max: 14, message: '账号长度6-14位', trigger: 'change'}
+    {trigger:'change',validator:validatorUserName}
+  ],
+  password: [
+    // {required: true, min: 5, max: 18, message: '密码长度5-18位', trigger: 'change'}
+    {trigger:'change',validator:validatorPassword}
+  ]
+}
+
 </script>
 
 <template>
@@ -37,13 +69,13 @@ const login = async () => {
     <el-row>
       <el-col :span="12" :xs="0"></el-col>
       <el-col :span="12" :xs="24">
-        <el-form class="login_form">
+        <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginForms">
           <h1>Hello</h1>
           <h2>欢迎</h2>
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
           </el-form-item>
           <el-form-item>
