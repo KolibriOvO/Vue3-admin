b461704681c4e5ea964da6cad9aac82e1f496630
kolibri <vigilanteovo@gmail.com>
2023-08-03 00:12:31 +0800

    feat: 菜单

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,21 +2,47 @@ export const constantRoute = [
   {
     path: '/login',
     component: () => import('@/views/login/index.vue'),
-    name: 'login'
+    name: 'login',
+    meta: {
+      title: '登录',
+      show: false
+    }
   },
   {
     path: '/',
-    component: () => import('@/views/home/index.vue'),
-    name: 'layout'
+    component: () => import('@/layout/index.vue'),
+    name: 'layout',
+    meta: {
+      title: 'layout',
+      show: true
+    },
+    children: [
+      {
+        path: '/home',
+        components: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '首页',
+          show: true
+        }
+      }
+    ]
   },
   {
     path: '/404',
     component: () => import('@/views/404/index.vue'),
-    name: '404'
+    name: '404',
+    meta: {
+      title: '404',
+      show: false
+    }
   },
   {
     path: '/:pathMatch(.*)*',
     redirect: '/404',
-    name: 'any'
+    name: 'any',
+    meta: {
+      title: '任意',
+      show: false
+    }
   }
 ]
