85563a18b4ebb605df2749b245f140ff6e4755f8
kolibri <vigilanteovo@gmail.com>
2023-08-02 17:26:37 +0800

    pinia

---
+++ src/store/modules/user.ts
@@ -0,0 +1,28 @@
+import {defineStore} from "pinia";
+import {reqLogin} from "@/api/user";
+import type {loginForm, loginResponseData} from "@/api/user/type.ts";
+import type {userState} from "@/store/modules/types/types.ts";
+import {GET_TOKEN, SET_TOKEN} from "@/utils/token.ts";
+
+let useUserStore = defineStore('User', {
+  state: (): userState => {
+    return {
+      token: GET_TOKEN(),
+    }
+  },
+  actions: {
+    async userLogin(data: loginForm) {
+      let result: loginResponseData = await reqLogin(data);
+      if (result.code == 200) {
+        this.token = result.data.token as string
+        SET_TOKEN(result.data.token as string)
+        return 'ok'
+      } else {
+        return Promise.reject(new Error(result.data.message))
+      }
+    }
+  },
+  getters: {}
+})
+
+export default useUserStore
