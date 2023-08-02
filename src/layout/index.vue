1090046c0d9ab20a98438368e72a646fc83821fa
kolibri <vigilanteovo@gmail.com>
2023-08-03 00:11:45 +0800

    主页

---
+++ src/layout/index.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import Logo from './logo/index.vue'
+import Menu from './menu/index.vue'
+import useUserStore from "@/store/modules/user.ts";
+
+let userStore = useUserStore()
+</script>
+
+<template>
+  <div class="layout_container">
+    <!--左侧菜单-->
+    <div class="layout_slider">
+      <Logo></Logo>
+      <el-scrollbar class="scrollbar">
+        <el-menu background-color="#001529" text-color="white">
+          <Menu :menuList="userStore.menuRoutes"></Menu>
+        </el-menu>
+      </el-scrollbar>
+    </div>
+    <!--顶部导航-->
+    <div class="layout_tabbar"></div>
+    <!--内容展示区-->
+    <div class="layout_main"></div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout_container {
+  width: 100%;
+  height: 100vh;
+
+  .layout_slider {
+    width: $base-menu-width;
+    height: 100vh;
+    background: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+
+      .el-menu {
+        border-right: none;
+      }
+    }
+  }
+
+  .layout_tabbar {
+    position: fixed;
+    top: 0;
+    left: $base-menu-width;
+    width: calc(100% - $base-menu-width);
+    height: $base-tabbar-height;
+  }
+
+  .layout_main {
+    position: absolute;
+    width: calc(100% - $base-menu-width);
+    height: calc(100vh - $base-tabbar-height);
+    background-color: yellowgreen;
+    left: $base-menu-width;
+    top: $base-tabbar-height;
+    padding: 20px;
+    overflow: auto;
+  }
+}
+</style>
